0401cd2aec1f664adfac9ccec6bcc4a1651dcad7
dhekalesanchita091-arch <dhekalesanchita091@gmail.com>
2026-06-30 21:18:10 +0530

    Add '/view' endpoint to retrieve student data
    Added a new endpoint '/view' to return a list of students.

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ app=FastAPI()
 @app.get('/')
 def get():
     return {"message":"This is my FastAPI app"}
+    @app.get("/view")
+def view():
+    students = [
+        {
+            "id": 1,
+            "name": "Sanchita",
+            "age": 19,
+            "course": "BSc CS"
+        },
+        {
+            "id": 2,
+            "name": "Prachiti",
+            "age": 20,
+            "course": "BSc DS"
+        }
+    ]
+
+    return students
